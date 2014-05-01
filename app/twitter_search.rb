a2e703a6cf9a6579e3bf71b6c6067d71ee4fcda2
nTraum <philipp.press@googlemail.com>
2014-05-01 19:16:51 +0200

    enable configuration via config file

--- app/twitter_search.rb
+++ app/twitter_search.rb
@@ -1,12 +1,19 @@
 require 'sinatra/base'
+require 'sinatra/config_file'
 require 'slim'
 
 
 class TwitterSearch < Sinatra::Application
+  register Sinatra::ConfigFile
+
+  config_file File.expand_path('../config/config.yml', __FILE__)
+
+  #
   get '/' do
     slim :index
   end
 
+  # Renders a generic 404 page when the requested URL could not be found
   not_found do
     slim :not_found
   end
