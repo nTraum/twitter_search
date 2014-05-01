4800881c7409fbcb2de894a171539c3e275fb331
nTraum <philipp.press@googlemail.com>
2014-05-01 19:23:12 +0200

    minor testing improvements

--- test/app/twitter_search_test.rb
+++ test/app/twitter_search_test.rb
@@ -6,9 +6,12 @@ describe 'twitter_search app' do
     last_response.ok?.must_equal true
   end
 
-  it 'must display 404 / not found' do
-    get '/path_that_does_not_exist'
-    last_response.not_found?.must_equal true
-    last_response.body.must_match /404/
+
+  describe 'error pages' do
+    it 'must display 404 / not found' do
+      get '/path_that_does_not_exist'
+      last_response.not_found?.must_equal true
+      last_response.body.must_match /404/
+    end
   end
 end
